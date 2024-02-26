8cb3edf8c11f993ec9ef03f47d18fd1ffa206ffc
Kamikali <addi98unity@gmail.com>
2024-02-26 18:57:37 +0100

    initial push

---
+++ satz_umformer.py
@@ -0,0 +1,74 @@
+import asyncio
+from openai import AsyncOpenAI
+from semeval_reader import entries as semeval_tuples
+
+client = AsyncOpenAI()
+
+
+async def main(semeval_tuple):
+
+    # ignore sentences with comments?
+    # if semeval_tuple['comment'] is not '' ...
+
+    system = f"""
+    Find the verb of the sentence that describes the relation between the nouns e1 and e2 and return its lemma.
+    """
+
+    prompt = semeval_tuple['sentence']
+
+    chat_completion = await client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                        "content": system
+            },
+            {
+                "role": "user",
+                        "content": prompt
+            }
+        ],
+        model="gpt-3.5-turbo-0125",
+    )
+    return chat_completion.choices[0].message.content
+
+
+def extract_verb(sentence):
+    # remove to, 3rd person s, ing...
+    if sentence[0:3] == 'to ':
+        return sentence[3::]
+    if sentence[-1] == 's':
+        return sentence[0:-1]
+    if sentence[-3:] == 'ing':
+        # test for double consonants before ing:
+        validate = sentence[-5:-3]
+        if validate[0] == validate[1]:
+            # remove one consonant
+            sentence = sentence[:-4] + sentence[-3:]
+        return sentence[:-3]
+    return sentence
+
+
+# tests
+print(extract_verb("to swim"))
+print(extract_verb("swim"))
+print(extract_verb("tolerate"))
+print(extract_verb("swims"))
+print(extract_verb("hissing"))
+
+verb_dict = dict()
+
+# get verb dict
+try:
+    for tuple in semeval_tuples:
+
+        data = asyncio.run(main(tuple))
+        print(f"\nSatz: {tuple['sentence']}")
+        print("GPT : " + data + "\n")
+        verb = extract_verb(data)
+
+        try:
+            verb_dict[verb] += 1
+        except:
+            verb_dict[verb] = 1
+except KeyboardInterrupt:
+    pass
---
+++ semeval_reader.py
@@ -0,0 +1,28 @@
+def parse_file_to_list(filename):
+    entries = []
+    entry = {}
+    count = 0
+
+    with open(filename, 'r', encoding='utf-8') as file:
+
+        for line in file:
+            line = line.strip()
+
+            if count % 4 == 0:  # satz
+                entry['sentence'] = line[6:-1]
+            if count % 4 == 1:  # semantische relation
+                entry['sem_rel'] = line
+            if count % 4 == 2:  # kommentar
+                entry['comment'] = line[8:-1]
+            if count % 4 == 3:  # leerzeile
+                if entry['sem_rel'] == 'Instrument-Agency(e2,e1)':
+                    entries.append(entry)
+                entry = {}
+
+            count += 1
+
+    return entries
+
+
+# Example usage (assuming the content is in 'input_file.txt'):
+entries = parse_file_to_list('semeval/training/TRAIN_FILE.TXT')
