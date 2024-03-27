315e7be7437ee2933e340f754a7053edb8e4fcd2
nilu nadaraj <n.nilushika7@gmail.com>
2024-03-27 14:54:31 +0100

    r

--- codebase/softmax_and_log_dicts.py
+++ codebase/softmax_and_log_dicts.py
@@ -1,14 +1,14 @@
 import math
-import counted_verb_dicts
+import dicts
 from softmax import softmax_dict
 
 LOG_DICTS = []
-for d in counted_verb_dicts.dict_original:
+for d in dicts.dict_original:
     LOG_DICTS.append(({key: math.log10(value)
                      for key, value in d[0].items()}, d[1]))
 
 SOFTMAXED_DICTS = []
-for d in counted_verb_dicts.dict_original:
+for d in dicts.dict_original:
     SOFTMAXED_DICTS.append((softmax_dict(d[0]), d[1]))
 
 for dic in SOFTMAXED_DICTS:
